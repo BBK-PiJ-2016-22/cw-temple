733454033e2f809bc1b61ac51f1c13aff6935a2b
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2017-04-12 20:41:10 +0100

    Code refractored

--- src/main/java/gui/Constants.java
+++ src/main/java/gui/Constants.java
@@ -1,15 +1,15 @@
 package main.java.gui;
 
 public interface Constants {
-  double GAME_WIDTH_PROP = 0.78;       //Width of the Smain.java.game portion (prop of total)
-  double GAME_HEIGHT_PROP = 1.0;      //Height of the Smain.java.game portion (prop of total)
+  double GAME_WIDTH_PROP = 0.78;       //Width of the main.java.game portion (prop of total)
+  double GAME_HEIGHT_PROP = 1.0;      //Height of the main.java.game portion (prop of total)
 
   //Dimensions of the error pane (in pixels)
   int ERROR_WIDTH = 500;
   int ERROR_HEIGHT = 150;
   double INFO_SIZE = 0.5;    //How much of the screen should the info make up?
 
-  //String ROOT = "/Users/keith/Courses/PiJ/coursework/cw-temple/code/src/Smain.java.images/";
+  //String ROOT = "/Users/keith/Courses/PiJ/coursework/cw-temple/code/src/images/";
   //replaced with:
     String ROOT = System.getProperty("user.dir")+"/src/main/java/images/";
 }
